9b7b4a22bb51dc78d4695db9c3124ca4a4b27831
Bilal Yusuf <bilalyusuf973@gmail.com>
2023-12-27 14:20:07 +0530

    fixed login related bugs.

--- components/layout/LeftBarProfileSection.tsx
+++ components/layout/LeftBarProfileSection.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { Avatar } from '@mui/material';
 
@@ -8,15 +8,20 @@ import useCurrentUser from '@/hooks/useCurrentUser';
 
 const LeftBarProfileSection = () => {
   const [showLogout, setShowLogout] = useState(false);
-  const { data } = useCurrentUser();
+  let userData: any;
+  useEffect(() => {
+      const { data }  = useCurrentUser();
+      userData = data;
+  }, [useCurrentUser])
+  
   return (
     <>
         <div className="profileSection hidden xl:flex ml-[6vw] m-3 p-2 cursor-pointer rounded-[20px] hover:bg-[#d5d5d5] dark:hover:bg-[#1c1c1c]" onClick={() => setShowLogout(prev => !prev)}>
             <Avatar alt="" src="" sx={{ width: 45, height: 45 }} />
             <div className='flex justify-between items-center w-full ml-2'>
                 <div className='flex flex-col leading-[20px]'>
-                    <span className='font-semibold'>{data?.currentUser?.name || 'Name'}</span>
-                    <span className='text-[#afafafbe]'>@{data?.currentUser?.username || 'username'}</span>
+                    <span className='font-semibold'>{userData?.currentUser?.name || 'Name'}</span>
+                    <span className='text-[#afafafbe]'>@{userData?.currentUser?.username || 'username'}</span>
                 </div>
                 <div className='font-extrabold text-xl mb-[7px]'>...</div>
             </div>
@@ -26,7 +31,7 @@ const LeftBarProfileSection = () => {
                 <div className='py-4'>
                     <div className='flex items-center gap-x-1 font-semibold px-5 py-2 hover:bg-[#dadada] dark:hover:bg-[#1b1b1b] cursor-pointer' onClick={() => signOut({callbackUrl: "/", redirect: true})}>
                         <span>Log out</span>
-                        <span>@{data?.currentUser?.username || 'username'}</span>
+                        <span>@{userData?.currentUser?.username || 'username'}</span>
                     </div>
                 </div>
             </div>
