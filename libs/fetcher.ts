ada33a432c8539545c2912dd684074e3e22300d2
Bilal Yusuf <bilalyusuf973@gmail.com>
2024-02-20 16:40:36 +0530

    Major bug fixes

--- libs/fetcher.ts
+++ libs/fetcher.ts
@@ -4,13 +4,13 @@ import { getSession } from 'next-auth/react';
 const fetcher = async (url: string) => {
     const session = await getSession();
 
-    const res = await axios.post(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
+    const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email: session?.user?.email })
-    });
+    };
+
+    const res = await axios.post(url, requestOptions);
     const data = await res.data;
     return data;
 }
