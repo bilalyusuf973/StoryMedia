5be7ca7d015f39c12dda0a620786fb25e3a52883
Bilal Yusuf <bilalyusuf973@gmail.com>
2024-02-21 00:26:31 +0530

    Major bug fixes

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -2,7 +2,7 @@ import useSWR from 'swr';
 import fetcher from '@/libs/fetcher';
 
 const useCurrentUser = () => {
-    const { data, error, isLoading } = useSWR('/api/auth/currentuser', fetcher);
+    const { data, error, isLoading } = useSWR('/api/currentuser', fetcher);
     return { data, error, isLoading };
 };
 
